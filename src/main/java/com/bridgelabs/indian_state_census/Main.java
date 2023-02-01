0d495893db2ef6419cc952c82c2d37d3f4f3fd52
Sourav2707 <smartsourav33@gmail.com>
2023-02-01 18:10:19 +0530

    Initial commit with welcome message

---
+++ src/main/java/com/bridgelabs/indian_state_census/Main.java
@@ -0,0 +1,7 @@
+package com.bridgelabs.indian_state_census;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian state census analyser program");
+    }
+}
